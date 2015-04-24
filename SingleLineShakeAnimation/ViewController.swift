8bc7887d9cc873b180a0ee426b06acc6e82df839
Håkon Bogen <hakon.bogen@shortcut.no>
2015-04-24 10:56:09 +0200

    Initial Commit

---
+++ SingleLineShakeAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingleLineShakeAnimation
+//
+//  Created by Håkon Bogen on 24/04/15.
+//  Copyright (c) 2015 haaakon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

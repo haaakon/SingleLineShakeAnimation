15449b0a3f8910356d2748e8b09827b7b3fba66d
Håkon Bogen <hakon.bogen@shortcut.no>
2015-05-05 20:33:13 +0200

    add workaround for accessability label on the button when shaking

--- UIView+Shake.swift
+++ UIView+Shake.swift
@@ -53,8 +53,24 @@ extension UIView {
     }
 
     public func postAccessabilityNotification(#text : String ) {
-        println(text)
-        UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification, text)
+        var hasRead = false
+        NSNotificationCenter.defaultCenter().addObserverForName(UIAccessibilityAnnouncementDidFinishNotification, object: nil , queue: nil) { (notification) -> Void in
+            if hasRead == false {
+                UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification, text)
+                hasRead = true
+                NSNotificationCenter.defaultCenter().removeObserver(self, name: UIAccessibilityAnnouncementDidFinishNotification, object:nil)
+            }
+        }
+        // seems to be a bug with UIAccessability that does not allow to post a notification with text in the action when tapping a button
+        dispatch(after:0.01) {
+            UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification, " ")
+        }
+    }
+
+    func didFinishReadingAccessabilityLabel() {
+        dispatch_async(dispatch_get_main_queue(), {
+            UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification, "hello world")
+        })
     }
 
     private func shake(#forTimes: Int, position: ShakePosition, durationPerShake: NSTimeInterval, completion: (() -> Void)?) {
@@ -81,6 +97,15 @@ extension UIView {
         }
     }
 
+}
+
+func dispatch(#after: NSTimeInterval, closure: dispatch_block_t) {
+    dispatch(after: after, closure: closure)
+}
+
+func dispatch(#after: NSTimeInterval, queue: dispatch_queue_t = dispatch_get_main_queue(), #closure: dispatch_block_t) {
+    let time = dispatch_time(DISPATCH_TIME_NOW, Int64(after) * Int64(NSEC_PER_SEC))
+    dispatch_after(time, dispatch_get_main_queue(), closure)
 }
 
 private struct ShakePosition  {
